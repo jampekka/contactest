a7c4bab782a1ce4f1e19fb5b13e544b9aa137e64
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2020-04-21 22:19:11 +0100

    Smoothing refactorings

--- contacts_by_age_and_place.py
+++ contacts_by_age_and_place.py
@@ -32,13 +32,21 @@ def get_contact_matrix(data, ag_participants=None):
         n = ag_participants[part_ag]
         for cnt_ag, cd in d.groupby('cnt_age_group'):
             M[part_ag, cnt_ag] += cd['contact_weight'].sum()/n
+    os = np.sum(M)
+    M = scipy.ndimage.gaussian_filter(M, 0.25)
+    M *= os/np.sum(M)
+    # Assume the result should be symmetric. TODO: Verify
+    #M += M.T; M /= 2 
     return M
 
 data = data[data[place_types].values.sum(axis=1) > 0]
 data['contact_weight'] = 1.0
 
-#M_total = get_contact_matrix(data)
-#M_cumtotal = np.zeros_like(M_total)
+M_total = get_contact_matrix(data)
+#os = np.sum(M_total)
+#M_total = scipy.ndimage.gaussian_filter(M_total, 0.25)
+#M_total *= os/np.sum(M_total)
+M_cumtotal = np.zeros_like(M_total)
 
 
 Ms = []
@@ -51,19 +59,20 @@ for country, cd in data.groupby('country'):
         
         OM = get_contact_matrix(d, ag_participants)
         # TODO: This isn't very well founded, but at least it gets rid of zeros
-        os = np.sum(OM)
-        OM = scipy.ndimage.gaussian_filter(OM, 0.25)
+        #os = np.sum(OM)
+        #OM = scipy.ndimage.gaussian_filter(OM, 0.25)
 
-        OM /= np.sum(OM)
-        OM *= os
+        #OM /= np.sum(OM)
+        #OM *= os
 
-        #M_cumtotal += OM
+        M_cumtotal += OM
         
         M = pd.DataFrame(OM, columns=ag_labels)
         M.insert(0, "participant_age", ag_labels)
         M.insert(0, "place_type", place_type)
         M.insert(0, "country", country)
         Ms.append(M)
+        #plt.title(place_type)
         #plt.imshow(OM, origin='lower', extent=(0, 80, 0, 80))
         #plt.show()
 
